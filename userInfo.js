447ba160f76847e71e3560e13271d9bdfe08c2ec
Gyimah Jessica Ankomah <jagyimah003@st.ug.edu.gh>
2024-06-27 22:35:15 +0000

    userInfo.js

---
+++ userInfo.js
@@ -0,0 +1,10 @@
+// Task 3: createUserProfiles function
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: index + 1
+    }));
+}
+
+module.exports = { createUserProfiles };
